e1bacb46eda0a064e61114a22a31b9a13382a99f
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-12-29 23:42:20 +0600

    customize the info data

--- component/Info.tsx
+++ component/Info.tsx
@@ -3,7 +3,25 @@ const Info = ({ style }: any) => {
     return (
         <>
             <h1>Hi, This is Pranto Chakraborty</h1>
-            <p className={style}>A passionate web developer. My core skill is based on React (frontend) and Laravel (backend) and I love to do most things using these technologies. I love to learn new technology, coding, play chess and cricket, travelling, listen to music, watch movies and tv series, and helps other. I graduated with a bachelor's degree in Computing from the University of Greenwich, in 2021. I am available for any kind of job opportunity that suits my interests.</p>
+            <p className={style}>
+                A dedicated and energetic full-stack web developer, who believes in quality work, seeking a position where I can share my knowledge to grow the company and enhance my skills.Always prioritizing work and business value and delivering the highest possible quality work.I have skills in MERN Stack development, frontend and backend web development on both sides and have been learning continuously to get better at this sector.
+            </p>
+            <p className={style}>
+                In my previous job experience, I learned I lot of things outside the web development process such as how to handle a team, builds project presentation with seniors and organise events etc.
+            </p>
+            <p className={style}>
+                Describe the skills that I work on:
+
+                -Frontend: HTML, CSS, Bootstrap, JavaScript, React, Tailwind CSS, WordPress
+
+                -Backend: Laravel, Livewire, Express JS, Rest API, Node JS, Node JS
+
+                -Database: MySQL, MongoDB
+
+                -Tools: GitHub, Figma, Photoshop, Trello, VSCode, Sublime, Firebase, NaviCat, Discord, Slack, JWT, Vercel, Netlify, Microsoft office,
+
+                -Extra-skills: cPanel managing, client support
+            </p>
         </>
     );
 };
--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -4,7 +4,9 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Navbar</a>
+                <Link className="navbar-brand" href={'/'}>
+                    Logo
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
